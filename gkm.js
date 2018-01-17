ac9b0dc446951b3c3bce833dd750d3dcdb1ab94d
Jonas Andersson <jonas@halvbra.se>
2018-01-17 14:22:02 +0100

    Add processOptions

--- gkm.js
+++ gkm.js
@@ -1,22 +1,20 @@
 'use strict';
 
-// TODO: Verify if we're getting loaded from multiples location and prevent creating new child processes?
+module.exports = (processOptions = {}) => {
+	var EventEmitter2 = require('eventemitter2').EventEmitter2;
+	var path = require('path');
+	var spawn = require('child_process').spawn;
 
-var EventEmitter2 = require('eventemitter2').EventEmitter2;
-var path = require('path');
-var spawn = require('child_process').spawn;
+	var events = new EventEmitter2({wildcard: true});
+	var gkm = spawn('java', ['-jar', path.join(__dirname, 'lib/gkm.jar')], processOptions);
 
-var events = new EventEmitter2({wildcard: true});
-var gkm = spawn('java', ['-jar', path.join(__dirname, 'lib/gkm.jar')]);
+	gkm.stdout.on('data', function(data) {
+		data = data.toString().split(/\r\n|\r|\n/).filter(function(item) { return item; });
+		for (var i in data) {
+			var parts = data[i].split(':');
+			events.emit(parts[0], parts.slice(1));
+        	}
+	});
 
-gkm.stdout.on('data', function(data) {
-	data = data.toString().split(/\r\n|\r|\n/).filter(function(item) { return item; });
-	for (var i in data) {
-		var parts = data[i].split(':');
-		events.emit(parts[0], parts.slice(1));
-	}
-});
-
-module.exports = {
-	events: events
+	return {events: events}
 };
